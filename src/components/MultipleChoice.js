982912d6c6a6d3b7cc184d9fc1013cadebbd5836
Andrew Y Xie <andyxie@D-108-179-177-42.dhcp4.washington.edu>
2017-05-15 18:49:06 -0700

    MultipleChoice worked

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
 class MultipleChoice extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: null,
+    };
+  }
+  onClick(id) {
+    this.setState({
+      selected: id,
+    })
+  }
+  submit() {
+    if(this.state.selected !== null){
+      this.props.submit(this.state.selected);
+    }
+  }
   renderItems() {
     const options = this.props.question.options;
     const listItems = options.map((item, key) =>
       <li
-        className={(key===this.props.question.optionSelected ? "list-group-item-success" : "")
+        className={(key===this.state.selected ? "list-group-item-success" : "")
                     + " list-group-item list-group-item-action "}
         key={key}
+        onClick={() => {this.onClick(key)}}
         >
         <a>{item}</a>
       </li>
@@ -19,10 +36,12 @@ class MultipleChoice extends Component {
   render() {
     return (
       <div className="container">
-      <div className="list-group-item">{this.props.question.content}</div>
-      <ul className="list-group">
-        {this.renderItems()}
-      </ul>
+        <ul className="list-group">
+          <li className="list-group-item">{this.props.question.content}</li>
+          {this.renderItems()}
+          <li className="btn btn-primary btn-lg" onClick={() => this.submit() }>Submit</li>
+        </ul>
+
       </div>
     );
   }
